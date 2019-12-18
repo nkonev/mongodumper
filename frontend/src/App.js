3b23e3d999e9f8db61201abbbe16f214e499f37e
Konev Nikita <konevnd@msk.bcs.ru>
2019-12-18 10:44:45 +0300

    fix header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -157,8 +157,8 @@ function App() {
         <div className="App">
             <div className={classes.root}>
                 <header className="App-header">
-                    <pre>mongorestore --drop --gzip --archive=/tmp/yourdump</pre>
-                    <pre>docker exec -i e7825dd4664b mongorestore --drop --gzip --archive &lt; /tmp/yourdump</pre>
+                    <div>mongorestore --drop --gzip --archive=/tmp/yourdump</div>
+                    <div>docker exec -i e7825dd4664b mongorestore --drop --gzip --archive &lt; /tmp/yourdump</div>
                 </header>
                 <List component="nav" aria-label="secondary mailbox folders">
                     {connections.map((value, index) => {
